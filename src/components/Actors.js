3615e53f990f8c35dfa0df5f246c437d0206ae35
northkuria <kuri4martin@gmail.com>
2022-07-27 09:30:14 +0300

    implement routes

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,26 @@
-import React from "react";
-import { actors } from "../data";
+import React from 'react'
+import { actors } from '../data'
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const myActors = actors.map((actor) => (
+    <div key={actor.name}>
+      <h2>Name :{actor.name}</h2>
+      Movies:
+      <ul>
+        {actor.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+  return (
+    <div>
+      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {myActors}
+    </div>
+  )
 }
 
-export default Actors;
+export default Actors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,26 @@
-import React from "react";
-import { movies } from "../data";
+import React from 'react'
+import { movies } from '../data'
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const myMovies = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>Name :{movie.title}</h2>
+      Time: {movie.time} minutes<br></br>
+      Genres:
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {myMovies}
+    </div>
+  )
 }
 
-export default Movies;
+export default Movies
